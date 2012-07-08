34205ea6cd40f8d52e387bca05088d1f4ce650ea
doclm <liam.magee@gmail.com>
2012-07-08 23:33:08 +1000

    Added configurable spec folder

--- tasks/jasmine-node-task.js
+++ tasks/jasmine-node-task.js
@@ -8,23 +8,83 @@
 
 module.exports = function(grunt) {
 
-  // Please see the grunt documentation for more information regarding task and
-  // helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
+    // Please see the grunt documentation for more information regarding task and
+    // helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
+    // ==========================================================================
+    // TASKS
+    // ==========================================================================
 
-  grunt.registerTask('jasmine_node', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('jasmine_node'));
-  });
+    grunt.registerMultiTask('jasmine_node', 'Your task description goes here.', function() {
+        var jasmine = require('jasmine-node')
+        var util,
+            Path= require('path');
+        try {
+            util = require('util')
+        } catch(e) {
+            util = require('sys')
+        }
 
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
 
-  grunt.registerHelper('jasmine_node', function() {
-    return 'jasmine-node-task!!!';
-  });
+        var specFolder = this.file.src;
+        var isVerbose = false;
+        var showColors = true;
+        var teamcity = process.env.TEAMCITY_PROJECT_NAME || false;
+        var useRequireJs = false;
+        var extentions = "js";
+        var match = '.';
+        var matchall = false;
+        var autotest = false;
+        var useHelpers = true;
+        var forceExit = false;
+
+        var junitreport = {
+            report: false,
+            savePath : "./reports/",
+            useDotNotation: true,
+            consolidate: true
+        }
+
+        var regExpSpec = new RegExp(match + (matchall ? "" : "spec\\.") + "(" + extentions + ")$", 'i')
+        var onComplete = function(runner, log) {
+            util.print('\n');
+            if (runner.results().failedCount == 0) {
+                exitCode = 0;
+            } else {
+                exitCode = 1;
+            }
+            if (forceExit) {
+                process.exit(exitCode);
+            }
+        };
+
+        try {
+            // Tell grunt this task is asynchronous.
+            var done = this.async();
+            // Your async code.
+            jasmine.executeSpecsInFolder(specFolder,
+                onComplete,
+                isVerbose,
+                showColors,
+                teamcity,
+                useRequireJs,
+                regExpSpec,
+                junitreport);
+
+
+        }
+        catch (e) {
+            console.log(e)
+        }
+        ;
+    });
+
+    // ==========================================================================
+    // HELPERS
+    // ==========================================================================
+
+    grunt.registerHelper('jasmine_node', function() {
+        //return 'jasmine-node-task!!!';
+    });
 
 };
